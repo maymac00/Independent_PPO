06031e0056d996e2209611ec2bff4e33e2dadd9a
arnau <arnau.mayoral10@gmail.com>
2023-10-20 10:35:08 +0200

    args_from_json

--- IndependentPPO/config.py
+++ IndependentPPO/config.py
@@ -81,7 +81,7 @@ def parse_ppo_args():
 
 def args_from_json(directory):
     def read_json_file(file_path):
-        with open(file_path + "/config.json", "r") as file:
+        with open(file_path, "r") as file:
             data = json.load(file)
         return data
 
