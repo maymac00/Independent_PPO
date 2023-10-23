0f28866e065f807a69ecc4a85d2d1c10b9528818
Mayo <arnau.mayoral10@gmail.com>
2023-10-23 16:06:39 +0200

    config i més abstracció

---
+++ update_lib.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /home/arnau/tools/python_venv/IndependentPPO/bin/activate
+pip uninstall multiagentethicalgathering
+pip install git+https://github.com/maymac00/MultiAgentEthicalGatheringGame.git
