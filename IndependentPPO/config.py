a2b0713eebf3857c1e573a800a8a2d6179d81f36
Mayo <arnau.mayoral10@gmail.com>
2023-10-27 09:51:21 +0200

    args_from_json now takes json as default values and adds cmd arguments over them

--- IndependentPPO/config.py
+++ IndependentPPO/config.py
@@ -6,22 +6,27 @@ import json
 from IndependentPPO.utils.misc import str2bool
 
 
+class CustomArgumentParser(argparse.ArgumentParser):
+    def parse_args(self, args=None, namespace=None):
+        result = super().parse_args(args, namespace)
+        for key, value in vars(result).copy().items():
+            if value is None:
+                delattr(result, key)
+        return result
+
+
 def parse_ppo_args():
     parser = argparse.ArgumentParser()
 
     # Logging    
     parser.add_argument("--verbose", type=str2bool, default=False, help="Stdout")
     parser.add_argument("--tb-log", type=str2bool, default=True, help="Tensorboard log")
-    # parser.add_argument("--wandb-log", type=str2bool, default=True, help="Wandb log")     # not sure if you use this
     parser.add_argument("--tag", type=str, default='IPPO_clip_gaussian', help="Training tag")
 
     # Environment
     parser.add_argument("--n-agents", type=int, default=2, help="N° of agents")
     parser.add_argument("--env", type=str, default="CommonsGame-v0", help="Gym environment")
-    # parser.add_argument("--n-envs", type=int, default=1, help="N° of envs")   # not sure if you use this
     parser.add_argument("--seed", type=int, default=1, help="seed of the experiment")
-    # parser.add_argument("--norm-obs", type=str2bool, default=True, help="Normalize observations")  # not sure if you use this
-    # parser.add_argument("--norm-rew", type=str2bool, default=True, help="Normalize rewards")   # not sure if you use this
 
     # Experiment
     parser.add_argument("--max-steps", type=int, default=500, help="Max n° of steps per episode")
@@ -59,13 +64,6 @@ def parse_ppo_args():
     # Metrics
     parser.add_argument("--last-n", type=int, default=500, help="Average last n metrics")
 
-    # wandb     # not sure if you use this
-    # parser.add_argument("--wandb-project-name", type=str, default="", help="Wandb's project name")
-    # parser.add_argument("--wandb-entity", type=str, default="", help="Entity (team) of wandb's project")
-    # parser.add_argument("--wandb-mode", type=str, default="offline",
-    #    help="online or offline wadb mode. if offline, we'll try to sync immediately after the run")
-    # parser.add_argument("--wandb-code", type=str2bool, default=False, help="Save code in wandb")
-
     # Torch
     parser.add_argument("--n-cpus", type=int, default=8, help="N° of cpus/max threads for process")
     parser.add_argument("--th-deterministic", type=str2bool, default=True,
@@ -89,11 +87,74 @@ def args_from_json(directory):
     config_data = read_json_file(directory)
 
     # Create an empty argparse.Namespace object
-    args = argparse.Namespace()
+    json_args = argparse.Namespace()
 
     # Update the argparse.Namespace object with the dictionary
     for key, value in config_data.items():
-        setattr(args, key, value)
+        setattr(json_args, key, value)
+
+    # Parse arguments
+    parser = CustomArgumentParser(description='Example')
+    # Logging
+    parser.add_argument("--verbose", type=str2bool, help="Stdout")
+    parser.add_argument("--tb-log", type=str2bool,  help="Tensorboard log")
+    parser.add_argument("--tag", type=str, help="Training tag")
+
+    # Environment
+    parser.add_argument("--n-agents", type=int, help="N° of agents")
+    parser.add_argument("--env", type=str,  help="Gym environment")
+    parser.add_argument("--seed", type=int,  help="seed of the experiment")
+
+    # Experiment
+    parser.add_argument("--max-steps", type=int,  help="Max n° of steps per episode")
+    parser.add_argument("--n-steps", type=int, help="Steps between policy updates")
+    parser.add_argument("--tot-steps", type=int,  help="Total timesteps of the experiment")
+    parser.add_argument("--early-stop", type=float,help="Steps to stop the experiment early")
+    parser.add_argument("--save-dir", type=str,
+                        help="Directory to save the model and metrics")
+
+    parser.add_argument("--past-actions-memory", type=int, help="Number of past actions to remember")
+
+    # Algorithm
+    parser.add_argument("--clip", type=float, help="Surrogate clipping coefficient")
+    parser.add_argument("--target-kl", type=float, help="Target KL divergence threshold")
+    parser.add_argument("--gamma", type=float, help="Discount factor")
+    parser.add_argument("--gae-lambda", type=float, help="Gae lambda")
+    parser.add_argument("--ent-coef", type=float, help="Entropy coefficient")
+    parser.add_argument("--v-coef", type=float, help="Value function coefficient")
+    parser.add_argument("--parallelize", type=str2bool, help="Parallelize the environment")
+    parser.add_argument("--n-envs", type=int, help="Parallelize the environment")
+
+    # Update
+    parser.add_argument("--actor-lr", type=float, help="Actor lr")
+    parser.add_argument("--critic-lr", type=float, help="Critic lr")
+    parser.add_argument("--anneal-lr", type=str2bool, help="Toggles annealing learning rates")
+    parser.add_argument("--n-epochs", type=int, help="N° of update epochs")
+    parser.add_argument("--norm-adv", type=str2bool, help="Toggles advantages normalization")
+    parser.add_argument("--max-grad-norm", type=float, help="Maximum norm for gradient clipping")
+    parser.add_argument("--critic-times", type=int, help="Multiplicator for number of epochs of the critic")
+
+    # DNN
+    parser.add_argument("--h-size", type=int, help="Layers size")
+    parser.add_argument("--h-layers", type=int, help="Number of layers")
+
+    # Metrics
+    parser.add_argument("--last-n", type=int, help="Average last n metrics")
+
+    # Torch
+    parser.add_argument("--n-cpus", type=int, help="N° of cpus/max threads for process")
+    parser.add_argument("--th-deterministic", type=str2bool,
+                        help="Toggles for `torch.backends.cudnn.deterministic=False`")
+    parser.add_argument("--cuda", type=str2bool, help="Toggles cuda")  # check .to(device) for GPUs
+
+    cmd_args = parser.parse_args()
+
+    # compare namespace objects and get cmd_args over json_args
+    args = vars(json_args)
+    for key, value in vars(cmd_args).items():
+        if value is not None:
+            args[key] = value
+
     return args
 
 
--- IndependentPPO/example.py
+++ IndependentPPO/example.py
@@ -5,49 +5,14 @@ pip install git+https://github.com/maymac00/MultiAgentEthicalGatheringGame.git
 from EthicalGatheringGame import MAEGG
 from EthicalGatheringGame.presets import tiny, small, medium, large
 from IndependentPPO.IPPO import IPPO
+from IndependentPPO.config import args_from_json
 import gym
 import matplotlib
 
 matplotlib.use('TkAgg')
 
 env = gym.make("MultiAgentEthicalGathering-v1", **tiny)
-args = {
-    "verbose": False,
-    "tb_log": True,
-    "tag": "example",
-    "env": "MultiAgentEthicalGathering-v1",
-    "seed": 1,
-    "max_steps": 500,
-    "n_agents": 2,
-    "n_steps": 2500,
-    "tot_steps": 25000000,
-    "save_dir": "example_data",
-    "early_stop": 25000000.0,
-    "past_actions_memory": 0,
-    "clip": 0.2,
-    "target_kl": None,
-    "gamma": 0.95,
-    "gae_lambda": 0.95,
-    "ent_coef": 0.2,
-    "v_coef": 0.5,
-    "actor_lr": 0.0003,
-    "critic_lr": 0.005,
-    "anneal_lr": True,
-    "n_epochs": 10,
-    "norm_adv": True,
-    "max_grad_norm": 1.0,
-    "critic_times": 2,
-    "h_size": 256,
-    "last_n": 500,
-    "n_cpus": 8,
-    "th_deterministic": True,
-    "cuda": False,
-    "batch_size": 2500,
-    "parallelize": True,
-    "n_envs": 5,
-    "h_layers": 2,
-}
-
+args = args_from_json("IndependentPPO/example_data/example/2500_30000_1_ckpt/config.json")
 ppo = IPPO(args, env=env)
 ppo.train()
 
