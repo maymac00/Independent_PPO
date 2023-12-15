a6cacbf39a3880d3582b2df0611ad476f0b02b2f
arnau <arnau.mayoral10@gmail.com>
2023-12-15 19:00:05 +0100

    find_policy changes

--- IndependentPPO/lr_schedules.py
+++ IndependentPPO/lr_schedules.py
@@ -16,7 +16,7 @@ class DefaultPPOAnnealing(LRScheduler):
         self.ppo.actor_lr = frac * self.ppo.init_args.actor_lr
         self.ppo.critic_lr = frac * self.ppo.init_args.critic_lr
 
-        for ag in self.ppo.agents:
+        for ag in self.ppo.agents.values():
             ag.a_optimizer.param_groups[0]["lr"] = frac * self.ppo.init_args.actor_lr
             ag.c_optimizer.param_groups[0]["lr"] = frac * self.ppo.init_args.critic_lr
 
