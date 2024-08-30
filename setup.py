633c61c3bf0df9490b6793bb6ab5547df2d0e517
arnau <arnau.mayoral10@gmail.com>
2024-08-30 17:44:56 +0200

    package specification

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='IPPO',
-      version='0.1',
-      install_requires=['gym', 'matplotlib', 'numpy', 'torch'],
-      packages=find_packages(),
-      )
